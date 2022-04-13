c31bbc48fc17aa14a8ae58bac5269e08a011dd11
Manu <gomanu6@gmail.com>
2022-04-13 20:44:03 +0530

    Moving active users

--- move_as/move_queue.sh
+++ move_as/move_queue.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-. ./move_folder.sh
-
-
 base_path="/mnt/shares/sftp/old_ms2/"
 old_src="All-Old/"
 active_source="as/ActiveServer/"
+users_active="users_active/"
 accounts_path="common_accounts/"
 old_users_path="old_user_folders/"
 archive_users_path="user_archives/"
 photos_archives="photos_archives/"
 photos_old="photos_old/"
 
-move_folders "${base_path}${old_src}" "${base_path}${accounts_path}" ./common_accounts.txt
+function move_folder () {
 
-move_folder "${base_path}${old_src}" "${base_path}${old_users_path}" ./old_user_folders.txt
-move_folder "${base_path}${old_src}" "${base_path}${archive_users_path}" ./user_archives.txt
-move_folder "${base_path}${old_src}" "${base_path}${photos_archives}" ./photos_archive.txt
-move_folder "${base_path}${old_src}" "${base_path}${photos_old}" ./photos_old.txt
+    source=$1
+    target=$2
+    list=$3
 
-move_folder "${base_path}${active_source}" "${base_path}${photos_old}" ./as_old_users.txt
+
+    while IFS= read -r line; do
+
+        mv "${source}${line}" "${target}"
+
+    done < "${list}"
+
+
+}
+
+
+move_folder "${base_path}${active_source}" "${base_path}${users_active}" ./as_active_users.txt
+move_folder "${base_path}${active_source}" "${base_path}${old_users_path}" ./as_to_old_users.txt
+move_folder "${base_path}${active_source}" "${base_path}${photos_old}" ./as_to_photos_old.txt
+move_folder "${base_path}${active_source}" "${base_path}${archive_users_path}" ./as_to_user_archives.txt
+
 
 
