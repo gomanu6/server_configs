68b33e862322298ab1467ab5e125e5a0b37cc43e
Manu <gomanu6@gmail.com>
2022-04-16 17:23:07 +0530

    Adding backup logic

--- backup/backup.sh
+++ backup/backup.sh
@@ -19,22 +19,30 @@ while IFS= read -r user; do
     source="${source_base}${user_folder_to_backup}/"
     dest="${dest_base}${user_folder_to_backup}/${todays_date}"
 
+    if [ -d "${dest}" ]; then
 
-    for i in {-1..-7}; do
+        for i in {-1..-7}; do
 
-        date_to_check=$(date -d "${todays_date} ${i} days" +%Y-%m-%d)
-        date_folder_to_check="${dest_base}${user_folder_to_backup}/${date_to_check}"
+            date_to_check=$(date -d "${todays_date} ${i} days" +%Y-%m-%d)
+            date_folder_to_check="${dest_base}${user_folder_to_backup}/${date_to_check}"
 
-        if [ -d "${date_folder_to_check}" ]; then
-            link_dest="${date_folder_to_check}"
-            break
-        fi
+            if [ -d "${date_folder_to_check}" ]; then
+                link_dest="--link-dest=${date_folder_to_check}"
+                break
+            fi
 
-    done
+        done
+    
+    else
+
+        mkdir -vp "${dest}"
+
+
+    fi
 
 
 
-    rsync -hazvib --suffix="_older" --link-dest=$link_dest $source $dest
+    rsync -hazvib --suffix="_older" $link_dest $source $dest
 
 
         
