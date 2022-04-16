9723809676f8c4d9f10454306276e56f1ca4d9e1
Manu <gomanu6@gmail.com>
2022-04-16 21:46:14 +0530

    Adding backup logic

--- backup/backup.sh
+++ backup/backup.sh
@@ -8,44 +8,135 @@ todays_date="$(date +%Y-%m-%d)"
 users_file="${users_to_backup}"
 
 
+function rsync_backup () {
+    local source=$1
+    local dest=$2
+    
+    local todays_date="$(date +%Y-%m-%d)"
+    local 
+    
+    if [ -z "$3" ]; then
+        link_dest=""
+    else
+        echo "[rsync_backup]: Setting the --link-dest parameter."
+        link_dest="--link-dest=$3/"
+        echo "[rsync_backup]: --link-dest parameter has been set."
+    fi
+    
+
+    echo "[backup]: Starting Backup for ${user}."
+    if rsync -hazvib --suffix="_older" ${link_dest} ${source} ${dest}; then
+        echo "[backup]: Backup for ${user} on ${todays_date} finished."
+    else
+        echo "[backup]: WARNING!! Backup for ${user} on ${todays_date} failed."
+
+    fi
 
 
-# link_dest=""
+}
+
+
+
+echo "[backup]: Reading users file for users to Backup"
 
 while IFS= read -r user; do
 
     user_folder_to_backup=${user}
 
     source="${source_base}${user_folder_to_backup}/"
-    dest="${dest_base}${user_folder_to_backup}/${todays_date}"
+    user_dest="${dest_base}${user_folder_to_backup}/"
 
-    if [ -d "${dest}" ]; then
+    dest="${user_dest}${todays_date}"
 
-        for i in {-1..-7}; do
+    echo "[backup]: Checking if ${user} folder exists."
+    if [ -d ${source} ]; then
+        echo "[backup]: ${user} folder exists."
 
-            date_to_check=$(date -d "${todays_date} ${i} days" +%Y-%m-%d)
-            date_folder_to_check="${dest_base}${user_folder_to_backup}/${date_to_check}"
+        echo "[backup]: Checking if ${user}'s backup destination exists."
+        if [ -d ${user_dest} ]; then
+            echo "[backup]: Backup destination for ${user} exists."
 
-            if [ -d "${date_folder_to_check}" ]; then
-                link_dest="--link-dest=${date_folder_to_check}"
-                break
+            echo "[backup]: Checking previous Backup's for ${user}...."
+
+            for i in {0..-15}; do
+
+                echo "[backup]: Checking last backup directory for ${user}"
+                
+                date_to_check=$(date -d "${todays_date} ${i} days" +%Y-%m-%d)
+                date_folder_to_check="${user_dest}${date_to_check}"
+
+                if [ -d "${date_folder_to_check}" ]; then
+                    link_dest="${date_folder_to_check}"
+                    echo "[backup]: Found backup directory for ${user} dated ${date_folder_to_check}. --link-dest parameter has been set"
+                    break
+                fi
+
+            done
+
+            rsync_backup "${source}" "${dest}" "${link_dest}"
+
+        else
+            echo "[backup]: WARNING !! Backup destination for ${user} does not exist."
+
+            echo "[backup]: Creating Backup destination for ${user}."
+            
+            if mkdir -vp ${user_dest}; then
+                echo "[backup]: Backup Destination for ${user} has been created."
+
+                if mkdir -vp ${dest}; then
+
+                    echo "[backup]: Backup Directory for ${user} created for ${todays_date}"
+
+                    echo "[backup]: Starting Backup. --link-dest parameter has not been set."
+
+                    rsync_backup "${source}" "${dest}"
+
+
+                else
+                    echo "[backup]: WARNING !! Problem creating Backup Directory for ${user} for ${todays_date}"
+                    exit 1
+
+                fi
+
+
+            else
+                echo "[backup]: WARNING !! Problem creating Backup Destination for ${user}."
+                exit 1
             fi
 
-        done
-    
+
+        fi
+
+
     else
-
-        mkdir -vp "${dest}"
-
+        echo "[backup]: WARNING !! ${user} folder Does NOT exist. Nothing to Backup"
+        exit 1
 
     fi
 
 
 
-    rsync -hazvib --suffix="_older" $link_dest $source $dest
 
 
+    if [ -d "${dest}" ]; then
+        echo "[backup]: Destination directory for ${user} for ${todays_date} exists"
+
+    
+    else
         
+        echo "[backup]: No backup directory found for ${user}. Creating directory for ${todays_date}"
+        if mkdir -vp "${dest}"; then
+            echo "[backup]: Created backup directory for ${user}"
+        fi
+
+    fi
+
+
+
+
+
+
+       
 
     done < "${users_file}"
 
