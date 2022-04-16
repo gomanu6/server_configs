a32d77c3b239c5055922bf6a1a1640fbe349b54d
Manu <gomanu6@gmail.com>
2022-04-16 17:02:22 +0530

    Adding backup logic

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
+. ../settings.config
+
 source_base="/mnt/test/"
 dest_base="/mnt/backup/"
 todays_date="$(date +%Y-%m-%d)"
-
-user_folder_to_backup=$1
-
-source="${source_base}${user_folder_to_backup}/"
-dest="${dest_base}${user_folder_to_backup}/${todays_date}"
-
-
-link_dest=""
-
-
-for i in {-1..-7}; do
-
-    date_to_check=$(date -d "${todays_date} $i days" +%Y-%m-%d)
-    date_folder_to_check="${dest_base}${date_to_check}"
-
-    if [ -d "${date_folder_to_check}" ]; then
-        link_dest="${date_folder_to_check}"
-        break
-    fi
-
-done
+users_file="${users_to_backup}"
 
 
 
 
+# link_dest=""
+
+while IFS= read -r user; do
+
+    user_folder_to_backup=${user}
+
+    source="${source_base}${user_folder_to_backup}/"
+    dest="${dest_base}${user_folder_to_backup}/${todays_date}"
+
+
+    for i in {-1..-7}; do
+
+        date_to_check=$(date -d "${todays_date} ${i} days" +%Y-%m-%d)
+        date_folder_to_check="${dest_base}${user_folder_to_backup}/${date_to_check}"
+
+        if [ -d "${date_folder_to_check}" ]; then
+            link_dest="${date_folder_to_check}"
+            break
+        fi
+
+    done
 
 
 
+    rsync -hazvib --suffix="_older" --link-dest=$link_dest $source $dest
 
 
-rsync -hazbiv --suffix="_older" --link-dest=$link_dest $source $dest
+        
+
+    done < "${users_file}"
+
+
+
