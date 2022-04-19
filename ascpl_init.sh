b01d81c2ad03d907c5ab0421bc14fdec65fad4f6
Manu <gomanu6@gmail.com>
2022-04-19 16:35:39 +0530

    Adding init code

--- ascpl_init.sh
+++ ascpl_init.sh
@@ -1,13 +1,55 @@
 #!/bin/bash
 
-Create directory for config
 
-create directlry for log File
+# Create directory for config
 
-create log File
+echo "[ascpl_init]: Checking if config directory exists."
+if [ -d "${config_dir}" ]; then
+    echo "[ascpl_init]: Config Directory exists" 
+else
+    echo "[ascpl_init]: Config Directory does not exist. Creating it"
+
+    if mkdir -vp  "${config_dir}"; then
+        echo "[ascpl_init]: Config Directory has been created"
+    else
+        echo "[ascpl_init]: WARNING !! Unable to create config directory"
+    fi
+fi
 
 
-create default user
+# create directory for log File
+echo "[ascpl_init]: Checking if log directory exists."
+if [ -d "${log_dir}" ]; then
+    echo "[ascpl_init]: Log Directory exists" 
+else
+    echo "[ascpl_init]: Log Directory does not exist. Creating it"
+
+    if mkdir -vp  "${log_dir}"; then
+        echo "[ascpl_init]: Log Directory has been created"
+    else
+        echo "[ascpl_init]: WARNING !! Unable to create log directory"
+    fi
+fi
+
+
+# Create log File
+echo "[ascpl_init]: Checking if log file exists."
+if [ -f "${log_file}" ]; then
+    echo "[ascpl_init]: Log file exists" 
+else
+    echo "[ascpl_init]: Log file does not exist. Creating it"
+
+    if touch  "${log_file}"; then
+        echo "[ascpl_init]: Log file has been created"
+    else
+        echo "[ascpl_init]: WARNING !! Unable to create log file"
+    fi
+fi
+
+
+
+# create default user
+
 
 create sftp user and sftp groups
 
---
+++ create_system_user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+function create_system_user () {
+
+
+    local username=$1
+    local home_dir=""
+    local user_shell=""
+    local append_groups=""
+
+
+
+
+
+
+
+
+}
