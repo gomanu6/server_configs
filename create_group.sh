d41b526a8ee980bd7dbec070c570e37de6a1f6b5
Manu <gomanu6@gmail.com>
2022-04-21 15:24:05 +0530

    Adding logic for init file

--- create_group.sh
+++ create_group.sh
@@ -7,11 +7,16 @@ function create_group () {
     local groupname=$1
     
 
-    echo "[create_group]: Checking if group ${groupname} exists and creating it"
+    echo "[create_group]: Checking if group ${groupname} exists."
 
-    if getent group "${groupname}" || groupadd "${groupname}"; then
-        echo "[create_group]:  Group for ${ groupname} Created"
+    if grep -wi "${groupname}" /etc/group; then
+        echo "[create_group]:  WARNING !! Group ${groupname} already exists."
     else
+        if groupadd "${groupname}"; then
+            echo "[create_group]: Group ${groupname} created successfully."
+        else
+            echo "[create_group]: WARNING !! Unable to create Group ${groupname}."
+        fi
         echo "[create_group]:  WARNING !! Unable to create Group ${groupname}"
     fi
 
