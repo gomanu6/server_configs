81edaa8019e35935449a3121adf4a8408b0417bb
Manu <gomanu6@gmail.com>
2022-08-15 21:29:02 +0530

    Adding command for migration

---
+++ migration/m4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+
+
+
+for dir in ${source}/*; do
+    echo
+    echo "Moving ${dir}"
+    cp -r "${dir}" "${dest}"
+    echo
+    echo "XXXX--- Finished ---- XXXX"
+    echo
+done
+
+
--- migration/move_directories.sh
+++ migration/move_directories.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 
-. ./dest_paths.txt
+. ./dest_paths
 . ./move.sh
 
 src_base="/mnt/mounts/sftp/sftp_files/files/qbt_drive/qbt/completed/courses_unsorted"
+file="/mnt/mounts/sftp/sftp_files/files/qbt_drive/qbt/completed/courses_unsorted/folders.txt"
+
+declare -a folders_array
+
+# for dir in ${src}/*; do
+
+
+# done
+
 
 
 while IFS= read -r directory; do
     
-    echo "${src_base}/${directory}" 
-    echo "${directory}_dest"
+    folders_array+=(${directory})
+    #echo "${src_base}/${directory}" 
+    #echo "${directory}_dest"
 
 
     #move "${src_base}/${directory}" "${directory}_dest"
 
-done
+done < "${file}"
+
+
 
 
 
