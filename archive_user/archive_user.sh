daded4d170e702695868e518ba208cba876dcecb
Manu <gomanu6@gmail.com>
2022-12-02 18:38:28 +0530

    Starting archive script

---
+++ archive_user/archive_user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# create backup of the folder
+# change folder permissions
+# Unmount users partition
+# edit fstab entry ?
+# 
+
+
+day=$(date +%Y%m%d)
+backup_stamp=$(date +%Y%m%d_%H%M%S)
+
+
+
+
+
+
+
