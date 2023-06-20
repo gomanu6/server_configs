07c8d940c7052941fdbaa57cb929686c6a92dc0d
Manu <gomanu6@gmail.com>
2023-06-20 17:38:09 +0530

    Minor changes to Backup library

--- backup_2023/dir_latest.sh
+++ backup_2023/dir_latest.sh
@@ -8,7 +8,7 @@ dir_latest() {
 
     local parent_dir=$1
 
-    local latest_sub_dir=$(ls -t "${parent_dir}" | head -n 1)
+    local latest_sub_dir=$(ls -t --group-directories-first "${parent_dir}" | head -n 1)
     
     if [ -n "${latest_sub_dir}" ]; then
         echo "$n The latest Directory in ${parent_dir} is ${latest_sub_dir}"
