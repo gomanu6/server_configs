d4e831c26a1976bd99f349479372ad5e0981e8ce
gomanu6 <gomanu6@gmail.com>
2023-08-15 14:55:20 +0530

    adding user archive settings

--- archive_user/archive_user.sh
+++ archive_user/archive_user.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-
-# create backup of the folder
+# check if source folder exists
+# check if destination folder exists
+# copy source folder to target
 # change folder permissions
 # Unmount users partition
 # edit fstab entry ?
 # 
 
+. ./settings_archive_user.config
 
 day=$(date +%Y%m%d)
 backup_stamp=$(date +%Y%m%d_%H%M%S)
 
+user="$1"
+source_folder="${source_base}/${user}"
+target_folder="${target_base}/${user}"
 
 
 
